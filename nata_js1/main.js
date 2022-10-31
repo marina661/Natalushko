e6a6b4253eef909c201c84df7e17a23be1efd0d6
marina661 <didenkomar661@gmail.com>
2022-10-31 17:49:16 -0400

    befor branch creater

---
+++ nata_js1/main.js
@@ -0,0 +1,42 @@
+console.log('marina');
+let a= 'hello';
+let b='owu';
+let c= 'com';
+let d= 'ua';
+let q=1;
+let w=10;
+let r=-999;
+let t=123;
+let y=3.14;
+let i=2.7;
+let o=16;
+let p= true;
+let f= false;
+
+let name='Marina';
+let sur='Natalushko';
+let sec='Ivanivna'
+let mes=`${sur} ${name} ${sec}`;
+
+let z = 100;
+let v = '100';
+let m = true;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(q);
+console.log(w);
+console.log(r);
+console.log(t);
+console.log(y);
+console.log(i);
+console.log(o);
+console.log(p);
+console.log(f);
+
+console.log(mes);
+console.log(typeof z,z);
+console.log(typeof v,v);
+console.log(typeof m,m);
